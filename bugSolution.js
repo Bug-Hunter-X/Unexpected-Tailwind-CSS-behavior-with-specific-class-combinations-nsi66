b8fa21296cdd8ddc9ace45d2306a367ae89b509b
BugHunter-X <askzenai@gmail.com>
2024-12-27 19:24:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// Solution: Adjusting class order or using a different approach
+<div class="p-4 bg-red-500 text-white">
+  <p class="font-bold text-4xl">This is some text.</p>
+</div>
+// Or:
+<div class="p-4 bg-red-500 text-white">
+  <p class="text-4xl font-bold">This is some text.</p>
+</div>
+```
